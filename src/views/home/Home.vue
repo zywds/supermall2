9cb96a5fe9c756c9543198c4da1ddab24af1dbae
zywds <736375819@qq.com>
2021-01-30 13:34:22 +0800

    封装swipper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,12 +7,20 @@
         </div>
       </template>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="(item,index) in banners" :key="index">
+        <a :href="item.link">
+          <img style="width:100%" :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
    import NavBar from 'components/common/navbar/NavBar'
    import {getHomeMultidata} from 'network/home.js'
+   import {Swiper,SwiperItem} from 'components/common/swiper'
    export default {
     name:'Home',
     data () {
@@ -22,12 +30,15 @@
       }
     },
     components:{
-      NavBar
+      NavBar,
+      Swiper,
+      SwiperItem
     },
     created(){
       getHomeMultidata().then(res => {
         //console.log(res);
         this.banners = res.data.banner.list
+        console.log(this.banners);
         this.dkeywords = res.data.dKeyword.list
       },error => {
         console.log(error);
